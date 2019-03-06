e7542f77f017db1f993962727618f616f9f84229
Pedro Moura <pedromoura@Pedros-MacBook-Pro.local>
2019-03-06 11:31:54 -0300

    Props - State - Constructor

--- src/App.js
+++ src/App.js
@@ -4,25 +4,41 @@ import './App.css';
 
 class MyButton extends Component {
   render(){
-    return <button>{this.props.label}</button>
+    return <button onClick={() => { this.props.handleClick(this.props.label); }}>{this.props.label}</button>
   }
 }
 
 class MyLabel extends Component {
   render(){
-    return <p>Antes da Props {this.props.text}</p>
+    return <p>Cliquei no: {this.props.text}</p>
   }
 }
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      labelText: '',
+    };
+
+    this.setLabelText = this.setLabelText.bind(this);
+  }
+
+  setLabelText(labelText) {
+    this.setState({ labelText });
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <MyLabel text="Escrevendo pelo App"/>
-        <MyButton label="Botão 1"/>
-        <MyButton label="Botão 2"/>
-        <MyButton label="Botão 3"/>
-        <MyButton label="Botão 4"/>
+        <MyLabel text={this.state.labelText}/>
+        <MyButton handleClick={this.setLabelText} label="Botão 1"/>
+        <MyButton handleClick={this.setLabelText} label="Botão 2"/>
+        <MyButton handleClick={this.setLabelText} label="Botão 3"/>
+        <MyButton handleClick={this.setLabelText} label="Botão 4"/>
       </div>
     );
   }
